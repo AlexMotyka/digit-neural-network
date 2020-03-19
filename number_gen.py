6ba756927de0278acb9d685891d7dfa1e1a74215
motykaalex@gmail.com <oliviatinios@gmail.com>
2020-03-19 12:46:36 -0400

    Revert "Add Kalev's number generator"
    This reverts commit ef2f7c928d303857a6538ac4f1f2b1ba72d4ff87.

--- number_gen.py
+++ number_gen.py
@@ -1,6 +1,8 @@
 from random import seed
 from random import randint
-import os
+
+# seed random number generator
+seed(1337)
 
 zero = [0,1,1,1,0,
         1,0,0,0,1,
@@ -106,106 +108,87 @@ nine = [0,1,1,1,0,
 number_array = [zero, one, two, three, four, five, six, seven, eight, nine]
 
 # creates a new file called Digits.txt
-file = open('correct_digits.txt', 'w')
+file = open('newDigits.txt', 'w')
 complete_number_list = []
 
-# seed random number generator
-seed(9853)
-
 # will make this many of each mutation type
-set_count = 50
-mutation_const = 75
-count = 0
-os.mkdir('digits_correct')
+set_count = 100
+
+#### MUTATION SET 1 ####
+# file.write("--------- Mutation 1's ---------" + '\n')
+# count1 = 0
+# for number in number_array:
+#     new_number_list = []
+#     for _ in range(set_count):
+#         new_number = []
+#         for x in number:
+#
+#             # if the number is a 0, then roll 0 to 100 to see if it mutates (> 90 means mutation)
+#             if x == 0:
+#                 rolled_val = randint(0, 100)
+#                 if rolled_val > 95:
+#                     new_number.append(1)
+#                 else:
+#                     new_number.append(0)
+#             else:
+#                 new_number.append(1)
+#         new_number_list.append(new_number)
+#         complete_number_list.append(new_number)
+#     file.write("number" + str(count1) + '\n')
+#     file.write(str(new_number_list) + '\n')
+#     count1 = count1 + 1
+
+#### MUTATION SET 2 ####
+# file.write("--------- Mutation 0's ---------" + '\n')
+# count2 = 0
+# for number in number_array:
+#     new_number_list = []
+#     for _ in range(set_count):
+#         new_number = []
+#         for x in number:
+#
+#             # if the number is a 1, then roll 0 to 100 to see if it mutates (> 90 means mutation)
+#             if x == 1:
+#                 rolled_val = randint(0, 100)
+#                 if rolled_val > 95:
+#                     new_number.append(0)
+#                 else:
+#                     new_number.append(1)
+#             else:
+#                 new_number.append(0)
+#         new_number_list.append(new_number)
+#         complete_number_list.append(new_number)
+#     file.write("number" + str(count2) + '\n')
+#     file.write(str(new_number_list) + '\n')
+#     count2 = count2 + 1
+
+#### MUTATION SET 3 ####
+file.write("--------- Mutation 0's and 1's ---------" + '\n')
+count3 = 0
 for number in number_array:
-    ind_file = open('digits_correct/' + str(count) + '_digits_correct.txt', 'w' )
     new_number_list = []
     for _ in range(set_count):
-        new_number_removed = []
-        new_number_added = []
-        removed_mutated_flag = 0
-        added_mutated_flag = 0
-        for index, value in enumerate(number):
+        new_number = []
+        for x in number:
 
-            # if the number is 0
-            if value == 1:
-
-                # can only change 1 --> 0 if its an 'end' digit
-                left_num = False
-                if index > 0:
-                    left_num = bool(number[index - 1] == 1)
-                up_num = False
-                if index > 4:
-                    up_num = bool(number[index - 5] == 1)
-                down_num = False
-                if index < 39:
-                    down_num = bool(number[index + 5] == 1)
-
-                right_num = False
-                if index < 44:
-                    right_num = bool(number[index + 1] == 1)
-
-                if ((left_num and not right_num and not up_num and not down_num) or
-                    (right_num and not left_num and not up_num and not down_num) or
-                    (down_num and not up_num and not left_num and not right_num) or
-                    (up_num and not down_num and not right_num and not left_num)) and removed_mutated_flag == 0:
-
-                    # roll a percent from 0 to 100
-                    rolled_val = randint(0, 100)
-
-                    # mutation chance is 1 - mutation_const
-                    if rolled_val > mutation_const:
-                        new_number_removed.append(0)
-                        removed_mutated_flag = 1
-                    else:
-                        new_number_removed.append(1)
+            # Mutation chance on both 0 and 1
+            if x == 1:
+                rolled_val = randint(0, 100)
+                if rolled_val > 98:
+                    new_number.append(0)
                 else:
-                    new_number_removed.append(1)
-            else:
-                new_number_removed.append(0)
-        for index, value in enumerate(number):
-
-            # if the number is 0
-            if value == 0:
-
-                # can only change 1 --> 0 if its an 'end' digit
-                left_num = False
-                if index > 0:
-                    left_num = bool(number[index - 1] == 1)
-                up_num = False
-                if index > 4:
-                    up_num = bool(number[index - 5] == 1)
-                down_num = False
-                if index < 39:
-                    down_num = bool(number[index + 5] == 1)
-
-                right_num = False
-                if index < 44:
-                    right_num = bool(number[index + 1] == 1)
-
-                if (left_num or right_num or up_num or down_num) and added_mutated_flag == 0:
-
-                    # roll a percent from 0 to 100
-                    rolled_val = randint(0, 100)
-
-                    # mutation chance is 1 - mutation_const
-                    if rolled_val > mutation_const:
-                        new_number_added.append(1)
-                        added_mutated_flag = 1
-                    else:
-                        new_number_added.append(0)
+                    new_number.append(1)
+            elif x == 0:
+                rolled_val = randint(0, 100)
+                if rolled_val > 98:
+                    new_number.append(1)
                 else:
-                    new_number_added.append(0)
-            else:
-                new_number_added.append(1)
-        new_number_list.append(new_number_removed)
-        new_number_list.append(new_number_added)
-        complete_number_list.append(new_number_removed)
-        complete_number_list.append(new_number_added)
-
-    count = count + 1
-    ind_file.write(str(new_number_list))
-    ind_file.close()
+                    new_number.append(0)
+        new_number_list.append(new_number)
+        complete_number_list.append(new_number)
+    file.write("number" + str(count3) + '\n')
+    file.write(str(new_number_list) + '\n')
+    count3 = count3 + 1
 
 file.write("--------- Complete List ---------" + '\n')
 file.write(str(complete_number_list) + '\n')
@@ -215,10 +198,10 @@ file.close()
 
 test = [0, 1, 1, 1, 0,
         1, 0, 0, 0, 1,
-        0, 0, 0, 0, 1,
-        0, 0, 0, 0, 1,
-        0, 0, 0, 1, 0,
-        0, 0, 0, 0, 1,
+        1, 0, 0, 0, 1,
+        1, 1, 0, 0, 1,
+        1, 1, 1, 1, 1,
         0, 0, 0, 0, 1,
         1, 0, 0, 0, 1,
-        0, 0, 1, 1, 0]
+        0, 0, 0, 0, 1,
+        0, 1, 1, 1, 0]
